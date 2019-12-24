a713a811695269d435c29ec7b83187b9c962dccd
xaviguasch <xaviguasch@gmail.com>
2019-12-24 21:42:45 +0100

    solved destructuring practice

--- src/index.js
+++ src/index.js
@@ -4,25 +4,39 @@ import ReactDOM from 'react-dom'
 
 import cars from './practice.js'
 
+const [honda, tesla] = cars
+
+const {
+    speedStats: { topSpeed: teslaTopSpeed }
+} = tesla
+
+const {
+    speedStats: { topSpeed: hondaTopSpeed }
+} = honda
+
+const {
+    coloursByPopularity: [teslaTopColour]
+} = tesla
+const {
+    coloursByPopularity: [hondaTopColour]
+} = honda
+
 ReactDOM.render(
-    <div>
-        <h1>prova</h1>
-        {/* <table>
-            <tr>
-                <th>Brand</th>
-                <th>Top Speed</th>
-            </tr>
-            <tr>
-                <td>{tesla.model}</td>
-                <td>{teslaTopSpeed}</td>
-                <td>{teslaTopColour}</td>
-            </tr>
-            <tr>
-                <td>{honda.model}</td>
-                <td>{hondaTopSpeed}</td>
-                <td>{hondaTopColour}</td>
-            </tr>
-        </table> */}
-    </div>,
+    <tbody>
+        <tr>
+            <th>Brand</th>
+            <th>Top Speed</th>
+        </tr>
+        <tr>
+            <td>{tesla.model}</td>
+            <td>{teslaTopSpeed}</td>
+            <td>{teslaTopColour}</td>
+        </tr>
+        <tr>
+            <td>{honda.model}</td>
+            <td>{hondaTopSpeed}</td>
+            <td>{hondaTopColour}</td>
+        </tr>
+    </tbody>,
     document.getElementById('root')
 )
